dd765d1cba48aa2319f560678779a1f3b207858c
Антон <bozhan89@gmail.com>
2018-03-30 15:57:18 +0300

    Начал вносить правки с финальной проверки. Остановился на задаче фонового цвета у изображений в hotels

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ var search_button = document.querySelector(".modal-button");
 
 var search_form = document.querySelector(".search-form");
 
+document.addEventListener("DOMContentLoaded", function(index_form_hide) {
+  search_form.classList.add("toggle-off");
+});
+
 search_button.addEventListener("click", function (index_form_toggle) {
   index_form_toggle.preventDefault();
   search_form.classList.toggle("toggle-off")
