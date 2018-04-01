ce53582eed2de14b0724d84b10c5c7d2d57a9bc9
Антон <bozhan89@gmail.com>
2018-04-01 14:25:43 +0300

    Внес все правки

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
-var search_button = document.querySelector(".modal-button");
+var modal_button = document.querySelector(".modal-button");
 
 var search_form = document.querySelector(".search-form");
+
+var date_in = search_form.querySelector("#in");
+
+var date_out = search_form.querySelector("#out");
+
+var adults = search_form.querySelector("#adults");
+
+var kids = search_form.querySelector("#kids");
 
 document.addEventListener("DOMContentLoaded", function(index_form_hide) {
   search_form.classList.add("toggle-off");
 });
 
-search_button.addEventListener("click", function (index_form_toggle) {
+modal_button.addEventListener("click", function (index_form_toggle) {
   index_form_toggle.preventDefault();
   search_form.classList.toggle("toggle-off")
 });
+
+search_form.addEventListener("submit", function (index_form_error) {
+  if (!date_in.value || !date_out.value || !kids.value || !adults.value) {
+    index_form_error.preventDefault();
+    search_form.classList.remove("search-form-error");
+    search_form.offsetWidth = search_form.offsetWidth;
+    search_form.classList.add("search-form-error");
+  }
+});
+
+
+
